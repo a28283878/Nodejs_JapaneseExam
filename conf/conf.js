565034f81c19414df1660ecdaa5ceaf400bf2e2d
a28283878 <a28283878@gmail.com>
2018-07-14 19:49:44 +0800

    0714

--- conf/conf.js
+++ conf/conf.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-        host: 'localhost', 
-        user: 'root',
-        password: '1234',
-        database:'japanese_test',
+        host: 'db4free.net', 
+        user: 'richard_lin',
+        password: '!QAZ2wsx',
+        database:'japanese_exam',
         port: 3306
     }
     // mysql: {
