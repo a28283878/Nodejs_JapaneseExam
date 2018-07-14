a13384a25896b3837a4721b7fd25638fa2773499
a28283878 <a28283878@gmail.com>
2018-07-14 16:57:25 +0800

    0714

---
+++ conf/conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: 'db4free.net', 
+        user: 'richard_lin',
+        password: '!QAZ2wsx',
+        database:'japanese_testexam',
+        port: 3306
+    }
+};
