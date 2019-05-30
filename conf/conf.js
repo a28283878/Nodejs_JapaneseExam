291add977e4809565a856a49917ecf8bd0257e68
a28283878 <a28283878@gmail.com>
2019-05-30 13:40:38 +0800

    change mysql server to freemysqlhosting

--- conf/conf.js
+++ conf/conf.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-        host: 'db4free.net', 
-        user: 'richard_lin',
-        password: '!QAZ2wsx',
-        database:'japanese_exam',
+        host: 'sql12.freemysqlhosting.net', 
+        user: 'sql12293923',
+        password: '1BN2jM4CSV',
+        database:'sql12293923',
         port: 3306
     }
     // mysql: {
