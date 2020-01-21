17e208afa4d347426b2cf665d7ecc96e8240a1d8
a28283878 <a28283878@gmail.com>
2020-01-21 20:01:09 +0800

    add mysql

---
+++ conf/conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mysql: {
+        host: 'us-cdbr-iron-east-05.cleardb.net', 
+        user: 'b7c2185d23613a',
+        password: 'e4a77403',
+        database:'heroku_f4e8c6a63eb4830',
+        port: 3306
+    }
+    // mysql: {
+    //     host: 'db4free.net', 
+    //     user: 'richard_lin',
+    //     password: '!QAZ2wsx',
+    //     database:'japanese_exam',
+    //     port: 3306
+    // }
+};
