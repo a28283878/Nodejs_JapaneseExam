2cc715b9c120c78423be6e399f4fc4dae7bc594f
a28283878 <a28283878@gmail.com>
2020-01-21 22:29:09 +0800

    fix database

--- conf/conf.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mysql: {
-        host: 'us-cdbr-iron-east-05.cleardb.net', 
-        user: 'b7c2185d23613a',
-        password: 'e4a77403',
-        database:'heroku_f4e8c6a63eb4830'
-    }
-};
