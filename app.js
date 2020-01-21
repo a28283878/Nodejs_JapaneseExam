971a7b670ab88b7b2e4c1ddb2b3e79f5c825828f
a28283878 <a28283878@gmail.com>
2020-01-21 20:52:02 +0800

    fix cleardb bug

--- app.js
+++ app.js
@@ -68,9 +68,3 @@ _checkAuth = (req, res) =>{
   }
   req.session.isAuth = true;
 }
-
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
